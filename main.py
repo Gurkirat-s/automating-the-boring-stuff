5a4345870c7a1be95f0fd31120d225c6374f7ca5
Gurkirat Singh <gurkiratsingh@hotmail.ca>
2020-07-18 00:20:33 -0400

    Added Calculator

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Test Code to accompany the book Automating the Boring Stuff with Python
+# Sample Calculator
 
+print('Python Calculator')
+
+num1 = input("")
+operator = input("")
+num2 = input("")
+
+if operator == '+':
+    print(int(num1) + int(num2))
+else:
+    print('Error')
