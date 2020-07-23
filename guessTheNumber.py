35d7e9456bec9a5e2e7a2d4affde42b49cfd51bc
Gurkirat Singh <gurkiratsingh@hotmail.ca>
2020-07-23 00:26:10 -0400

    Guess The Number Game

---
+++ guessTheNumber.py
@@ -0,0 +1,23 @@
+import random
+
+num = random.randint(1,50)
+
+guess = int(input('Guess a number between 1 and 50: '))
+guesses = 0
+while True:
+    if guess < num:
+        print('Your guess is too low.')
+        guesses += 1
+    elif guess > num:
+        print('Your guess is too high')
+        guesses += 1
+    elif guess == num:
+        print('Congratulations! Your guess is correct\n')
+        guesses += 1
+        print('You guessed the correct number in ' + str(guesses) + ' guesses.')
+        break
+    guess = int(input('Guess another number: '))
+
+
+
+
